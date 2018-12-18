e0ac0fb45f1965b1040b7b8be6084a2bfbb95a46
balagopalanAS <balagopalan.a@terralogic.com>
2018-12-18 15:22:53 +0530

    Added the logo, favicon and filtered the siderbar according to requirement

--- billingUi/src/app/app.module.ts
+++ billingUi/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { FooterCommonComponent } from './footer-common/footer-common.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FooterComponent,
-    FooterCommonComponent
+    AppComponent
+   
   ],
   imports: [
     BrowserModule,
