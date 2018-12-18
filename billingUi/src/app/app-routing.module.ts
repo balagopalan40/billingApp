9559a786bac8c5617dfdfea675a6195168c639d3
balagopalanAS <balagopalan.a@terralogic.com>
2018-12-18 16:27:17 +0530

    added the routes and two components to navigate

--- billingUi/src/app/app-routing.module.ts
+++ billingUi/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+
+	{
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
